2e0ed71765cb6636d9ad44cc67d9394da8c41ce8
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2025-02-13 09:48:44 +0900

    Update error handling with Sentry integration and logging

--- pkg/server/error.go
+++ pkg/server/error.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net/http"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/m-mizutani/goerr/v2"
 	"github.com/secmon-lab/warren/pkg/utils/logging"
 )
@@ -14,7 +15,15 @@ var (
 func handleError(w http.ResponseWriter, r *http.Request, err error) {
 	logger := logging.From(r.Context())
 
-	logger.Error("Request error", "error", err)
+	hub := sentry.CurrentHub().Clone()
+	hub.ConfigureScope(func(scope *sentry.Scope) {
+		for k, v := range goerr.Values(err) {
+			scope.SetExtra(k, v)
+		}
+	})
+	evID := hub.CaptureException(err)
+
+	logger.Error("Request error", "error", err, "sentry.id", evID)
 
 	switch {
 	case goerr.HasTag(err, errBadRequest):
