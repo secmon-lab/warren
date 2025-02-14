a4b47b13e284789ca732fda1920385aa5184e4b7
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2025-02-15 06:23:02 +0900

    Update Gemini model value to "gemini-2.0-flash".

--- pkg/cli/config/gemini.go
+++ pkg/cli/config/gemini.go
@@ -22,7 +22,7 @@ func (x *GeminiCfg) Flags() []cli.Flag {
 			Usage:       "Gemini model",
 			Destination: &x.model,
 			Category:    "Gemini",
-			Value:       "gemini-2.0-flash-exp",
+			Value:       "gemini-2.0-flash",
 		},
 		&cli.StringFlag{
 			Name:        "gemini-project-id",
