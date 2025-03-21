0ce6accf20b95a91ac988b7dc799669aa97f0402
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2025-03-21 11:45:53 +0900

    wip

---
+++ pkg/domain/model/source/source.go
@@ -0,0 +1,105 @@
+package source
+
+import (
+	"context"
+	"time"
+
+	"github.com/m-mizutani/goerr/v2"
+	"github.com/secmon-lab/warren/pkg/domain/interfaces"
+	"github.com/secmon-lab/warren/pkg/domain/model/alert"
+	"github.com/secmon-lab/warren/pkg/domain/model/slack"
+	"github.com/secmon-lab/warren/pkg/domain/types"
+	"github.com/secmon-lab/warren/pkg/utils/logging"
+	"github.com/secmon-lab/warren/pkg/utils/thread"
+)
+
+type Source func(ctx context.Context, repo interfaces.Repository) ([]alert.Alert, error)
+
+func Thread(slackThread slack.Thread) Source {
+	return func(ctx context.Context, repo interfaces.Repository) ([]alert.Alert, error) {
+		thread.Reply(ctx, "🤖 Getting alerts from slack thread...")
+
+		alertList, err := repo.GetAlertListByThread(ctx, slackThread)
+		if err != nil {
+			return nil, err
+		}
+		if alertList == nil {
+			return nil, nil
+		}
+		alerts, err := repo.BatchGetAlerts(ctx, alertList.AlertIDs)
+		if err != nil {
+			return nil, err
+		}
+		return alerts, nil
+	}
+}
+
+func LatestAlertList(slackThread slack.Thread) Source {
+	return func(ctx context.Context, repo interfaces.Repository) ([]alert.Alert, error) {
+		thread.Reply(ctx, "🤖 Getting latest alerts from slack thread")
+		logging.From(ctx).Info("Getting latest alerts from slack thread", "slack_thread", slackThread)
+
+		alertList, err := repo.GetLatestAlertListInThread(ctx, slackThread)
+		if err != nil {
+			return nil, err
+		}
+		if alertList == nil {
+			return nil, goerr.New("no alert list found in this thread", goerr.V("slack_thread", slackThread))
+		}
+		alerts, err := repo.BatchGetAlerts(ctx, alertList.AlertIDs)
+		if err != nil {
+			return nil, err
+		}
+		return alerts, nil
+	}
+}
+
+func AlertListID(alertListID types.AlertListID) Source {
+	return func(ctx context.Context, repo interfaces.Repository) ([]alert.Alert, error) {
+		thread.Reply(ctx, "🤖 Getting alerts from alert list: "+alertListID.String())
+
+		alertList, err := repo.GetAlertList(ctx, alertListID)
+		if err != nil {
+			return nil, err
+		}
+		if alertList == nil {
+			return nil, nil
+		}
+		alerts, err := repo.BatchGetAlerts(ctx, alertList.AlertIDs)
+		if err != nil {
+			return nil, err
+		}
+		return alerts, nil
+	}
+}
+
+func Span(begin, end time.Time) Source {
+	return func(ctx context.Context, repo interfaces.Repository) ([]alert.Alert, error) {
+		thread.Reply(ctx, "🤖 Getting alerts from span: "+begin.Format("2006-01-02 15:04")+" to "+end.Format("2006-01-02 15:04"))
+
+		alerts, err := repo.GetAlertsBySpan(ctx, begin, end)
+		if err != nil {
+			return nil, err
+		}
+		return alerts, nil
+	}
+}
+
+func Static(alerts []alert.Alert) Source {
+	return func(ctx context.Context, repo interfaces.Repository) ([]alert.Alert, error) {
+		return alerts, nil
+	}
+}
+
+func Unresolved() Source {
+	return func(ctx context.Context, repo interfaces.Repository) ([]alert.Alert, error) {
+		thread.Reply(ctx, "🤖 Getting unresolved alerts...")
+
+		alerts, err := repo.GetAlertsWithoutStatus(ctx, types.AlertStatusResolved)
+		if err != nil {
+			return nil, goerr.Wrap(err, "failed to get alerts without resolved status")
+		}
+
+		return alerts, nil
+	}
+}
---
+++ pkg/usecase/interface.go
@@ -0,0 +1,27 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/secmon-lab/warren/pkg/domain/model/alert"
+	"github.com/secmon-lab/warren/pkg/domain/model/slack"
+)
+
+type SlackEvent interface {
+	// Slack event handlers
+	HandleSlackMessage(ctx context.Context, slackThread slack.Thread, text string, user slack.User, ts string) error
+	HandleSlackAppMention(ctx context.Context, user slack.User, mention slack.Mention, slackThread slack.Thread) error
+}
+
+type SlackInteraction interface {
+	// Slack interaction handlers
+	HandleSlackInteractionViewSubmissionResolveAlert(ctx context.Context, user slack.User, metadata string, values slack.StateValue) error
+	HandleSlackInteractionViewSubmissionResolveList(ctx context.Context, user slack.User, metadata string, values slack.StateValue) error
+	HandleSlackInteractionViewSubmissionIgnoreList(ctx context.Context, metadata string, values slack.StateValue) error
+	HandleSlackInteractionBlockActions(ctx context.Context, user slack.User, slackThread slack.Thread, actionID slack.ActionID, value, triggerID string) error
+}
+
+type Alert interface {
+	// Alert related handlers
+	HandleAlertWithAuth(ctx context.Context, schema string, alertData any) ([]*alert.Alert, error)
+}
