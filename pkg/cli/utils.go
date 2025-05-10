08457683169159d371e31c47c75a75dcbefa93e8
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2025-05-10 09:31:29 +0900

    refactor(cli): move builtinTool and toolList definitions from tool.go to utils.go to improve code organization and maintainability chore(cli): remove tool.go as it is no longer needed after refactoring

--- pkg/cli/utils.go
+++ pkg/cli/utils.go
@@ -1,6 +1,19 @@
 package cli
 
-import "github.com/urfave/cli/v3"
+import (
+	"context"
+	"log/slog"
+
+	"github.com/m-mizutani/gollem"
+	"github.com/secmon-lab/warren/pkg/domain/model/errs"
+	"github.com/secmon-lab/warren/pkg/tool/abusech"
+	"github.com/secmon-lab/warren/pkg/tool/ipdb"
+	"github.com/secmon-lab/warren/pkg/tool/otx"
+	"github.com/secmon-lab/warren/pkg/tool/shodan"
+	"github.com/secmon-lab/warren/pkg/tool/urlscan"
+	"github.com/secmon-lab/warren/pkg/tool/vt"
+	"github.com/urfave/cli/v3"
+)
 
 func joinFlags(flags ...[]cli.Flag) []cli.Flag {
 	var result []cli.Flag
@@ -8,4 +21,54 @@ func joinFlags(flags ...[]cli.Flag) []cli.Flag {
 		result = append(result, flag...)
 	}
 	return result
+}
+
+type builtinTool interface {
+	Name() string
+	Flags() []cli.Flag
+	Configure(ctx context.Context) error
+	LogValue() slog.Value
+	gollem.ToolSet
+}
+
+type toolList []builtinTool
+
+var tools = toolList{
+	&urlscan.Action{},
+	&otx.Action{},
+	&vt.Action{},
+	&shodan.Action{},
+	&abusech.Action{},
+	&ipdb.Action{},
+	// &bigquery.Action{},
+}
+
+func (x toolList) Flags() []cli.Flag {
+	flags := []cli.Flag{}
+	for _, tool := range x {
+		flags = append(flags, tool.Flags()...)
+	}
+	return flags
+}
+
+func (x toolList) LogValue() slog.Value {
+	var attrs []slog.Attr
+	for _, tool := range x {
+		attrs = append(attrs, slog.Any(tool.Name(), tool.LogValue()))
+	}
+	return slog.GroupValue(attrs...)
+}
+
+func (x toolList) ToolSets(ctx context.Context) ([]gollem.ToolSet, error) {
+	toolSets := []gollem.ToolSet{}
+	for _, tool := range x {
+		if err := tool.Configure(ctx); err != nil {
+			if err == errs.ErrActionUnavailable {
+				continue
+			}
+			return nil, err
+		}
+		toolSets = append(toolSets, tool)
+	}
+	return toolSets, nil
 }
