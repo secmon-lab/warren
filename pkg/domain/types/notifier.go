0c9e06f83379a1e0ad445e7500d99e712cf27309
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2025-10-25 04:55:58 +0900

    chore(notifier.go): remove unused notifier.go file to clean up the codebase

--- pkg/domain/types/notifier.go
+++
@@ -1,7 +0,0 @@
-package types
-
-import "context"
-
-// ProcessNotifier is a function type for notifying process events throughout the pipeline
-// Implementations should format messages appropriately for their output destination
-type ProcessNotifier func(ctx context.Context, message string)
